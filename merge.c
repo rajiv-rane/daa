c7f84d39597acbe63dd53694bc822796013311df
Rajiv Rane <120100767+rajiv-rane@users.noreply.github.com>
2024-06-12 21:10:57 +0530

    Update merge.c

--- merge.c
+++ merge.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-// Merges two subarrays of arr[].
-// First subarray is arr[l..m]
-// Second subarray is arr[m+1..r]
+
 
 void merge(int arr[], int l, int m, int r)
 {
@@ -19,7 +17,6 @@ void merge(int arr[], int l, int m, int r)
     for (j = 0; j < n2; j++)
         R[j] = arr[m + 1 + j];
     // Merge the temp arrays back
-    // into arr[l..r]
     // Initial index of first subarray
     i = 0;
     // Initial index of second subarray
@@ -58,24 +55,19 @@ void merge(int arr[], int l, int m, int r)
         k++;
     }
 }
-// l is for left index and r is
-// right index of the sub-array
-// of arr to be sorted
+
 void mergeSort(int arr[], int l, int r)
 {
     if (l < r)
-    {
-        // Same as (l+r)/2, but avoids
-        // overflow for large l and r
-        int m = l + (r - l) / 2;
-        // Sort first and second halves
+    {       
+        int m = l + (r - l) / 2;        
         mergeSort(arr, l, m);
         mergeSort(arr, m + 1, r);
         merge(arr, l, m, r);
     }
 }
 
-// Function to print an array
+
 void printArray(int A[], int size)
 {
     int i;
